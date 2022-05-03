83b2c9c4f680e0bc3d39faca7f13eddac81c1f3b
granadalook <tiven17.jsga@gmail.com>
2022-05-02 20:48:54 -0500

    fixed/correccion: se corrige typo   de texto modal que se encontraba mal escrito  y se le da formato al documento

--- ejercicio-sofka/src/TodoForm/index.js
+++ ejercicio-sofka/src/TodoForm/index.js
@@ -1,14 +1,11 @@
-import React from 'react';
-import { TodoContext } from '../TodoContext';
-import './TodoForm.css';
+import React from "react";
+import { TodoContext } from "../TodoContext";
+import "./TodoForm.css";
 
 function TodoForm() {
-  const [newTodoValue, setNewTodoValue] = React.useState('');
-  const {
-    addTodo,
-    setOpenModal,
-  } = React.useContext(TodoContext);
-  
+  const [newTodoValue, setNewTodoValue] = React.useState("");
+  const { addTodo, setOpenModal } = React.useContext(TodoContext);
+
   const onChange = (event) => {
     setNewTodoValue(event.target.value);
   };
@@ -27,20 +24,17 @@ function TodoForm() {
       <textarea
         value={newTodoValue}
         onChange={onChange}
-        placeholder="Cortar la cebolla oara el almuerzo"
+        placeholder="Cortar la cebolla para el almuerzo"
       />
       <div className="TodoForm-buttonContainer">
         <button
           type="button"
           className="TodoForm-button TodoForm-button--cancel"
           onClick={onCancel}
-          >
+        >
           Cancelar
         </button>
-        <button
-          type="submit"
-          className="TodoForm-button TodoForm-button--add"
-        >
+        <button type="submit" className="TodoForm-button TodoForm-button--add">
           Añadir
         </button>
       </div>
