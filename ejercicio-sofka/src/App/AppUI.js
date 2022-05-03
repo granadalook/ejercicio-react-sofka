e176d00a53e5f3437cc31dfa574325cc0ee266d2
granadalook <tiven17.jsga@gmail.com>
2022-05-02 21:55:38 -0500

    fixed: se incorpora boostrap al proyecto y se hace un componente explicito para  el todo  asi darle organizacion al proyecto

--- ejercicio-sofka/src/App/AppUI.js
+++ ejercicio-sofka/src/App/AppUI.js
@@ -1,55 +1,16 @@
 import React from "react";
-import { TodoContext } from "../TodoContext";
-import { TodoCounter } from "../TodoCounter";
-import { TodoSearch } from "../TodoSearch";
-import { TodoList } from "../TodoList";
-import { TodoItem } from "../TodoItem";
-import { TodoForm } from "../TodoForm";
-import { CreateTodoButton } from "../CreateTodoButton";
-import { Modal } from "../Modal";
+
 import Home from "./home/home";
+import Navbar from "./Navbar/Navbar";
+import TodoComponent from "./TodoComponent/TodoComponent";
 
 function AppUI() {
-  const {
-    error,
-    loading,
-    searchedTodos,
-    completeTodo,
-    deleteTodo,
-    openModal,
-    setOpenModal,
-  } = React.useContext(TodoContext);
-
   return (
-    <React.Fragment>
+    <div>
+      <Navbar />
+      <TodoComponent />
       <Home />
-      <TodoCounter />
-      <TodoSearch />
-
-      <TodoList>
-        {error && <p>Desespérate, hubo un error...</p>}
-        {loading && <p>Estamos cargando, no desesperes...</p>}
-        {!loading && !searchedTodos.length && <p>¡Crea tu primer TODO!</p>}
-
-        {searchedTodos.map((todo) => (
-          <TodoItem
-            key={todo.text}
-            text={todo.text}
-            completed={todo.completed}
-            onComplete={() => completeTodo(todo.text)}
-            onDelete={() => deleteTodo(todo.text)}
-          />
-        ))}
-      </TodoList>
-
-      {!!openModal && (
-        <Modal>
-          <TodoForm />
-        </Modal>
-      )}
-
-      <CreateTodoButton setOpenModal={setOpenModal} />
-    </React.Fragment>
+    </div>
   );
 }
 
